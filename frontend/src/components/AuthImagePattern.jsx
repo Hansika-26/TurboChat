4a55b7c9462ebc433e31588bdc6394e9512cf83b
Hansika-26 <IT23140998@my.sliit.lk>
2025-12-19 00:56:21 +0530

    Update signup page UI

---
+++ frontend/src/components/AuthImagePattern.jsx
@@ -0,0 +1,33 @@
+const AuthImagePattern = ({ title, subtitle }) => {
+  return (
+    <div
+      className="hidden lg:flex items-center justify-center p-12 relative bg-no-repeat bg-center"
+      style={{
+        backgroundImage: "url(/auth-bg.jpg)",
+        backgroundSize: "180%", // 👈 controls image size
+      }}
+    >
+      {/* dark overlay so grid & text are clear */}
+      <div className="absolute inset-0 bg-base-300/90"></div>
+
+      {/* content */}
+      <div className="relative max-w-md text-center">
+        <div className="grid grid-cols-10 gap-3 mb-20">
+          {[...Array(9)].map((_, i) => (
+            <div
+              key={i}
+              className={`aspect-square rounded-2xl bg-primary/20 ${
+                i % 2 === 0 ? "animate-pulse" : ""
+              }`}
+            />
+          ))}
+        </div>
+
+        <h2 className="text-2xl font-bold mb-4">{title}</h2>
+        <p className="text-base-content/60">{subtitle}</p>
+      </div>
+    </div>
+  );
+};
+
+export default AuthImagePattern;
